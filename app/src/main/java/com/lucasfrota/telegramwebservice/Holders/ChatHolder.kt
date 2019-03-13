2a1f0b71943c933feb122c6abb6a6bea168c9596
Lucas Frota <lucv.frota@gmail.com>
2019-03-13 14:40:17 -0400

    6 - Criando a classe ChatHolder

---
+++ app/src/main/java/com/lucasfrota/telegramwebservice/Holders/ChatHolder.kt
@@ -0,0 +1,7 @@
+package com.lucasfrota.telegramwebservice.Holders
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ChatHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+}

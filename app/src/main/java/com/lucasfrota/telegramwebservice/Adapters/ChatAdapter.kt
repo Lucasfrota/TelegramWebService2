78415f6934067a6142051114e4f44df4108950ba
Lucas Frota <lucv.frota@gmail.com>
2019-03-13 15:52:18 -0400

    7 - Criando e editando a classe ChatAdapter

---
+++ app/src/main/java/com/lucasfrota/telegramwebservice/Adapters/ChatAdapter.kt
@@ -0,0 +1,24 @@
+package com.lucasfrota.telegramwebservice.Adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.lucasfrota.telegramwebservice.Holders.ChatHolder
+import com.lucasfrota.telegramwebservice.Model.ChatItem
+import com.lucasfrota.telegramwebservice.R
+
+class ChatAdapter(private val itens: List<ChatItem>): RecyclerView.Adapter<ChatHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.chat_item, parent, false)
+        return ChatHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itens.size
+    }
+
+    override fun onBindViewHolder(holder: ChatHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}

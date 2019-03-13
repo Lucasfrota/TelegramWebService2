663e7121c25ae5437affbcd346656f8f763c4b41
Lucas Frota <lucv.frota@gmail.com>
2019-03-13 17:12:36 -0400

    11 - Pegando dados do servidor

--- app/src/main/java/com/lucasfrota/telegramwebservice/Activities/MainActivity.kt
+++ app/src/main/java/com/lucasfrota/telegramwebservice/Activities/MainActivity.kt
@@ -8,13 +8,19 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.textclassifier.TextLinks
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.lucasfrota.telegramwebservice.Adapters.ChatAdapter
 import com.lucasfrota.telegramwebservice.Model.ChatItem
 import com.lucasfrota.telegramwebservice.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -47,11 +53,30 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     private fun setUpRecyclerView() {
-        itens.add(ChatItem("Discussão sobre ETs", "Os ETs existem", "23:53"))
-        itens.add(ChatItem("fã clube do ET bilu", "A terra é convexa", "16:23"))
 
-        val adapter = ChatAdapter(itens)
-        recyclerView.adapter = adapter
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://us-central1-telegram-f88df.cloudfunctions.net/app/getChats"
+
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            Response.Listener<String>{ response ->
+
+                val chats = JSONArray(response)
+
+                for(i in 0 until chats.length()){
+                    val chat = chats.getJSONObject(i)
+                    itens.add(ChatItem(chat.getString("chatSubject"),
+                                       chat.getString("chatLastMessageSent"),
+                                       chat.getString("chatTimeStamp")))
+                }
+
+                val adapter = ChatAdapter(itens)
+                recyclerView.adapter = adapter
+            },
+            Response.ErrorListener { error ->
+
+            })
+
+        queue.add(stringRequest)
     }
 
     override fun onBackPressed() {
